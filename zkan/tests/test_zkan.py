2ea5d82cc0a4abdafa0e80a898d45ba7c9f09b1c
Tom Wood <tom.wood@civicactions.com>
2020-08-22 17:24:01 -0400

    One more fix.

--- zkan/tests/test_zkan.py
+++ zkan/tests/test_zkan.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
-    assert response.json() == dict(message="Hello world")
+    assert response.json() == dict(message="Hello World")
